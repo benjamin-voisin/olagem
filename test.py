7d6b22a60aa16f1d2728c5405b3131552b614073
Vneb23 <benjamin.voisin@lilo.org>
2022-10-25 22:31:33 +0200

    Programme python qui affiche un adj random

---
+++ test.py
@@ -0,0 +1,24 @@
+#Ce programme affiche un adjectif au hasard parmi l’ensemble des adjectifs de wikidata !!!
+import requests
+import json
+import random
+
+
+x = str(random.random())
+comment = "#" + x + "\n"
+
+url = 'https://query.wikidata.org/sparql'
+query = '''SELECT DISTINCT ?lexemeId ?lemma ?catLabel WHERE {
+  ?lexemeId dct:language wd:Q150;
+            wikibase:lexicalCategory wd:Q34698;
+            wikibase:lemma ?lemma.
+   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+ORDER BY UUID()
+LIMIT 1'''
+
+query = comment + query
+
+resp = str(requests.get(url, params={'query': query, 'format':'json'}).json())
+jason = json.loads(str(resp.replace("'",'"')))
+print (jason["results"]["bindings"][0]["lemma"]["value"])
