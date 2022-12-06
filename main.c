6f6497536d94451c8b73da8c51e30e311fa034cf
Vneb23 <benjamin.voisin@lilo.org>
2022-12-06 16:56:18 +0100

    Ça ressemble à un truc

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "lua.h"
+#include "lualib.h"
 #include "lauxlib.h"
 
 int main(){
@@ -6,14 +7,18 @@ int main(){
 	// Initialize the state
 	lua_State *L = luaL_newstate();
 	// load the librairies
-	//luaL_openlibs(L);
+	luaL_openlibs(L);
 	// do file
 	luaL_dofile(L,"generateur.lua");
 
 	lua_getglobal(L, "text");
-	const char* text = lua_tolstring(L, -1,NULL);
-
-	printf("%s\n", text);
+	if (lua_isnil(L,-1)){
+		printf("Cette variable n’est pas définie dans le code lua!\n");
+	}
+	else{
+		const char* text = lua_tolstring(L, -1,NULL);
+		printf("%s\n", text);
+	}
 
 	return 1;
 }
