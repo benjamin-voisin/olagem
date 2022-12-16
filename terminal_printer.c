1ae0de70162677e3451e46d63f992ae8524e7dd9
Vneb23 <benjamin.voisin@lilo.org>
2022-12-16 09:48:15 +0100

    Refarctor

--- terminal_printer.c
+++ terminal_printer.c
@@ -6,44 +6,51 @@
 #include <stdio.h>
 
 void color_init(){
-	init_pair(1, COLOR_BLACK, COLOR_BLACK);
-	init_pair(2, COLOR_WHITE, COLOR_BLACK);
-	init_pair(3, COLOR_GREEN, COLOR_BLACK);
-	init_pair(4, COLOR_RED, COLOR_BLACK);
-}
-
-void start_screen(const char* text) {
-
-	char * phrase;
-
-
-	uint8_t ch;
-	int y, x;
-
-	initscr();			/* Start curses mode 		  */
 	start_color();
 	if(has_colors() == FALSE){
 		endwin();
 		printf("Your terminal does not support color\n");
 		exit(1);
 	}
+	init_pair(1, COLOR_BLACK, COLOR_BLACK);
+	init_pair(2, COLOR_WHITE, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	init_pair(4, COLOR_RED, COLOR_BLACK);
+}
+
+void suppr(int x,int y){
+	getyx(stdscr, y, x);
+	attron(COLOR_PAIR(1));
+	move(y, x-1);
+	addch(' ');
+	move(y,x-1);
+	attrset(COLOR_PAIR(2));
+}
+
+void start_screen(const char* text) {
+
+
+	uint8_t phrase[strlen(text)];
+	int indice = 0;
+
+	uint8_t ch;
+	int y, x;
+
+	initscr();			/* Start curses mode 		  */
 	color_init();
 	cbreak(); 		/* supprime le buffer du terminal pour avoir les caractères instant */
 	noecho();		// évite d’écrire les caractères qu’on tape (comme ça c’est nous qui gérons
 
 	printw(text);
+	addch(phrase[indice]);
+	addch(*text);
 	refresh();
 	attron(COLOR_PAIR(2));
 	while(1){
 		ch = getch();
 		if (ch == 127){
-			getyx(stdscr, y, x);
-			attron(COLOR_PAIR(1));
-			move(y, x-1);
-			addch(' ');
-			move(y,x-1);
-			attrset(COLOR_PAIR(2));
-		}
+			suppr(x,y);
+					}
 		else{
 			addch(ch);
 		}
