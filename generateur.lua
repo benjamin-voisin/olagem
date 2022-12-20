f43f5b90a1dc3fbf6e8cdc8fab8a39f5e321cd08
Vneb23 <benjamin.voisin@lilo.org>
2022-12-20 16:57:38 +0100

    now equivallent to 10 fast fingers

--- generateur.lua
+++ generateur.lua
@@ -3,6 +3,17 @@ require "math"
 math.randomseed(os.time())
 
 function sentence_generator()
+	nb_words_per_sentences = 10
+	words = {"le", "de", "un", "être", "et", "à", "il", "avoir", "ne", "je", "son", "que", "se", "qui", "ce", "dans", "en", "du", "elle", "au", "de", "ce", "le", "pour", "pas", "que", "vous", "par", "sur", "faire", "plus", "dire", "me", "on", "mon", "lui", "nous", "comme", "mais", "pouvoir", "avec", "tout", "y", "aller", "voir", "en", "bien", "où", "sans", "tu", "ou", "leur", "homme", "si", "deux", "mari", "moi", "vouloir", "te", "femme", "venir", "quand", "grand", "celui", "si", "notre", "devoir", "là", "jour", "prendre", "même", "votre", "tout", "rien", "petit", "encore", "aussi", "quelque", "dont", "tout", "donner", "temps", "ça", "peu", "même", "falloir", "sous", "parler", "alors", "main", "chose", "ton", "mettre", "vie", "savoir", "yeux", "passer", "autre", "après", "regarder", "toujours", "puis", "jamais", "cela", "aimer", "non", "heure", "croire", "cent"}
+	text = words[math.random(#words)]
+	for k = 1, nb_words_per_sentences do
+		text = text.." "..words[math.random(#words)]
+	end
+	return text
+
+end
+
+function cadavres_exquis()
 	noms = {"Le cadavre", "Le chat", "Le chien", "La grenouille"}
 	noms.taille = 4
 	adjectifs1 = {"exquis", "noir", "pouilleux", "gluant"}
@@ -16,4 +27,3 @@ function sentence_generator()
 	phrase = noms[math.random(noms.taille)].." "..adjectifs1[math.random(adjectifs1.taille)].." "..verbes[math.random(verbes.taille)].." "..complements[math.random(complements.taille)].." "..adjectifs2[math.random(adjectifs2.taille)].."."
 	return phrase
 end
-
