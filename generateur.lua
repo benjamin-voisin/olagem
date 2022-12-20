f7db027bb5743d4689971d90842b278fa98f82bc
Vneb23 <benjamin.voisin@lilo.org>
2022-12-20 15:35:22 +0100

    generateur en mode cadavre exquis (mieux pour tests)

--- generateur.lua
+++ generateur.lua
@@ -3,14 +3,17 @@ require "math"
 math.randomseed(os.time())
 
 function sentence_generator()
-	n = math.random(5)
-	tableau = {
-		"Première phrase\n",
-		"Deuxième phrase\n",
-		"Troisième phrase\n",
-		"Quatrième phrase\n",
-		"Cinquième phrase\n"
-	}
-	return tableau[n]
+	noms = {"Le cadavre", "Le chat", "Le chien", "La grenouille"}
+	noms.taille = 4
+	adjectifs1 = {"exquis", "noir", "pouilleux", "gluant"}
+	adjectifs1.taille = 4
+	verbes = {"boira", "mange", "aime", "capture"}
+	verbes.taille = 4
+	complements = {"le vin", "un rat", "un os", "un moustique"}
+	complements.taille = 4
+	adjectifs2 = {"nouveau", "gris", "moelleux", "agressif"}
+	adjectifs2.taille = 4
+	phrase = noms[math.random(noms.taille)].." "..adjectifs1[math.random(adjectifs1.taille)].." "..verbes[math.random(verbes.taille)].." "..complements[math.random(complements.taille)].." "..adjectifs2[math.random(adjectifs2.taille)].."\n"
+	return phrase
 end
 
