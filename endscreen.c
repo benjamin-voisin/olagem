2cc5205fb7739d07d703a2a1f4bb102ad366621a
Vneb23 <benjamin.voisin@lilo.org>
2022-12-21 17:35:03 +0100

    long int

--- endscreen.c
+++ endscreen.c
@@ -6,7 +6,7 @@ bool end_screen(long int number_of_caracteres, long int time){
 	clear();
 	uint8_t ch;
 	long int number_of_words = (number_of_caracteres/4) * (60/time);
-	printw("You have typed %d caracteres in %ld seconds\n\n", number_of_caracteres, time); 
+	printw("You have typed %ld caracteres in %ld seconds\n\n", number_of_caracteres, time); 
 	printw("This translate to %ld WPM on 10FastFingers\n\n", number_of_words);
 	printw("Press r to restart the game. Press q to quit");
 	ch = getch();
