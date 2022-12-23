fd05207c05f5c681614d6ede83d1413cb7c3de4b
Vneb23 <benjamin.voisin@lilo.org>
2022-12-23 09:10:24 +0100

    reading settings from file

--- settings.c
+++ settings.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 long int max_time(){
-	return 60;
+	FILE * fichier;
+	fichier = fopen("settings.txt", "r");
+	if (fichier == NULL){
+		perror("fopen");
+		exit(1);
+	}
+	char letter;
+	char time_char[]= "ffff";
+	int i = 0;
+	while((letter = fgetc(fichier)) != '=');
+	while((letter = fgetc(fichier)) != EOF){
+		printf("%c", letter);
+		time_char[i] = letter;
+		i ++;
+	}
+	return (atoi(time_char));
 }
