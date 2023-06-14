34ff942323c34bbe052c7fecdbc8d761dc2d5274
Benjamin V <benjamin.voisin@ens-rennes.fr>
2023-06-14 13:24:31 +0200

    corrections

--- settings.c
+++ settings.c
@@ -1,24 +1,25 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ncurses.h>
 
-long int max_time(){
-	FILE * fichier;
-	fichier = fopen("settings.txt", "r");
-	if (fichier == NULL){
-		perror("fopen");
-		endwin();
-		exit(1);
-	}
-	char letter;
-	char time_char[]= "ffff";
-	int i = 0;
-	while((letter = fgetc(fichier)) != '=');
-	while((letter = fgetc(fichier)) != EOF){
-		time_char[i] = letter;
-		i ++;
-	}
-	fclose(fichier);
-	return (atoi(time_char));
+long int max_time() {
+  FILE *fichier;
+  fichier = fopen("/usr/share/olagem/settings.txt", "r");
+  if (fichier == NULL) {
+    perror("fopen");
+    endwin();
+    exit(1);
+  }
+  char letter;
+  char time_char[] = "ffff";
+  int i = 0;
+  while ((letter = fgetc(fichier)) != '=')
+    ;
+  while ((letter = fgetc(fichier)) != EOF) {
+    time_char[i] = letter;
+    i++;
+  }
+  fclose(fichier);
+  return (atoi(time_char));
 }
